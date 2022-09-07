30813ca1f845ae7030854fe2c3ff7273fffdbce0
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-09-07 15:29:45 +0300

    trying the code

--- src/developerCareer/WebApi/Controllers/ProgrammingLanguagesController.cs
+++ src/developerCareer/WebApi/Controllers/ProgrammingLanguagesController.cs
@@ -6,6 +6,7 @@ using Application.Features.ProgrammingLanguages.Models;
 using Application.Features.ProgrammingLanguages.Queries.GetByIdProgrammingLanguage;
 using Application.Features.ProgrammingLanguages.Queries.GetListProgrammingLanguage;
 using Core.Application.Requests;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Controllers.Base;
 
@@ -13,33 +14,37 @@ namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProgrammingLanguagesController : BaseController
+    public class ProgrammingLanguagesController : ControllerBase
     {
-        [HttpPost("Add")]
+        private readonly IMediator _mediator;
+
+        public ProgrammingLanguagesController(IMediator mediator) => _mediator = mediator;
+
+        [HttpPut("Add")]
         public async Task<IActionResult> Add([FromBody] CreateProgrammingLanguageCommand createProgrammingLanguage)
         {
-            CreatedProgrammingLanguageDto createPl = await Mediator.Send(createProgrammingLanguage);
+            CreatedProgrammingLanguageDto createPl = await _mediator.Send(createProgrammingLanguage);
             return Created("", createPl);
         }
 
         [HttpDelete("Remove")]
         public async Task<IActionResult> Delete([FromBody] RemoveProgrammingLanguageCommand removeProgrammingLanguage)
         {
-            RemovedProgrammingLanguageDto removePl = await Mediator.Send(removeProgrammingLanguage);
-            return Created("", removePl);
+            RemovedProgrammingLanguageDto removePl = await _mediator.Send(removeProgrammingLanguage);
+            return Ok(removePl);
         }
 
         [HttpPost("Update")]
         public async Task<IActionResult> Update([FromBody] UpdateProgrammingLanguageCommand updateProgrammingLanguage)
         {
-            UpdatedProgrammingLanguageDto updatePl = await Mediator.Send(updateProgrammingLanguage);
+            UpdatedProgrammingLanguageDto updatePl = await _mediator.Send(updateProgrammingLanguage);
             return Created("", updatePl);
         }
 
         [HttpGet("GetById{Id}")]
         public async Task<IActionResult> GetById([FromRoute] GetByIdProgrammingLanguageQuery getByIdPlQuery)
         {
-            ProgrammingLanguageGetByIdDto programmingLanguageGetByIdDto = await Mediator.Send(getByIdPlQuery);
+            ProgrammingLanguageGetByIdDto programmingLanguageGetByIdDto = await _mediator.Send(getByIdPlQuery);
 
             return Ok(programmingLanguageGetByIdDto);
         }
@@ -49,7 +54,7 @@ namespace WebApi.Controllers
         {
             GetListProgrammingLanguageQuery getListProgrammingLanguageQuery = new() { PageRequest = pageRequest };
 
-            ProgrammingLanguageListModel programmingLanguageListModel = await Mediator.Send(getListProgrammingLanguageQuery);
+            ProgrammingLanguageListModel programmingLanguageListModel = await _mediator.Send(getListProgrammingLanguageQuery);
 
             return Ok(programmingLanguageListModel);
         }
