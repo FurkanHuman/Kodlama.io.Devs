42f0d09cd1001e40c12e15a1ea1987d9e674b859
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-09-10 23:00:34 +0300

    dynamic query strucre added

---
+++ src/developerCareer/Application/Features/ProgrammingLanguages/Queries/GetListProgrammingLanguageByDynamic/GetListProgrammingLanguageByDynamicQuery.cs
@@ -0,0 +1,13 @@
+﻿using Application.Features.ProgrammingLanguages.Models;
+using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using MediatR;
+
+namespace Application.Features.ProgrammingLanguages.Queries.GetListProgrammingLanguage
+{
+    public class GetListProgrammingLanguageByDynamicQuery : IRequest<ProgrammingLanguageListModel>
+    {
+        public PageRequest PageRequest { get; set; }
+        public Dynamic Dynamic { get; set; }
+    }
+}
---
+++ src/developerCareer/Application/Features/ProgrammingLanguages/Queries/GetListProgrammingLanguageByDynamic/GetListProgrammingLanguageByDynamicQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Application.Features.ProgrammingLanguages.Models;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.ProgrammingLanguages.Queries.GetListProgrammingLanguage
+{
+    public class GetListProgrammingLanguageByDynamicQueryHandler : IRequestHandler<GetListProgrammingLanguageByDynamicQuery, ProgrammingLanguageListModel>
+    {
+        private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+        private readonly IMapper _mapper;
+
+        public GetListProgrammingLanguageByDynamicQueryHandler(IProgrammingLanguageRepository programmingLanguageRepository, IMapper mapper)
+        {
+            _programmingLanguageRepository = programmingLanguageRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ProgrammingLanguageListModel> Handle(GetListProgrammingLanguageByDynamicQuery request, CancellationToken cancellationToken)
+        {
+            IPaginate<ProgrammingLanguage> paginate = await _programmingLanguageRepository.GetListByDynamicAsync(request.Dynamic, index: request.PageRequest.Page, size: request.PageRequest.PageSize);
+            return _mapper.Map<ProgrammingLanguageListModel>(paginate); 
+        }
+    }
+}
