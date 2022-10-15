698ddf8ed81128abca7e5c96bb57d74d76483b85
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-10-15 03:08:58 +0300

    refactoring codes for ProgrammingTechnology

---
+++ src/developerCareer/Application/Services/AltServices/ProgrammingTechnologyService/IProgrammingTechnologyServisce.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.AltServices.ProgrammingTechnologyService
+{
+    public interface IProgrammingTechnologyService
+    {
+        Task<ProgrammingLanguage?> GetProgrammingLanguageById(int programmingLanguageId);
+    }
+}
---
+++ src/developerCareer/Application/Services/AltServices/ProgrammingTechnologyService/ProgrammingTechnologyManager.cs
@@ -0,0 +1,20 @@
+﻿using Application.Services.Repositories;
+using Domain.Entities;
+
+namespace Application.Services.AltServices.ProgrammingTechnologyService
+{
+    public class ProgrammingTechnologyManager : IProgrammingTechnologyService
+    {
+        private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+
+        public ProgrammingTechnologyManager(IProgrammingLanguageRepository programmingLanguageRepository)
+        {
+            _programmingLanguageRepository = programmingLanguageRepository;
+        }
+
+        public async Task<ProgrammingLanguage?> GetProgrammingLanguageById(int programmingLanguageId)
+        {
+           return await _programmingLanguageRepository.GetAsync(p => p.Id == programmingLanguageId);
+        }
+    }
+}
